8f291fe68cc98432873b680abc606ef919be0536
FrenchMontana10 <cecilofori1@gmail.com>
2022-10-01 02:30:53 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * @argc: argument count
+ *    * @argv: value
+ *     * Return: 0 Always.
+ */
+
+int main(int argc, char *argv[]  __attribute__((unused)))
+{
+
+		if (argc > 0)
+					printf("%d\n", argc - 1);
+			return (0);
+}
